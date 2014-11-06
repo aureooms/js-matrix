3d65b0083ad7597af61bcf0bfc6d04c152f1b509
aureooms <aurelien.ooms@gmail.com>
2014-11-06 22:35:05 +0100

    fill test

---
+++ test/js/src/fill.js
@@ -0,0 +1,41 @@
+
+var run, array, alloc;
+
+array = require( "aureooms-js-array" );
+
+alloc = matrix.__alloc__( array.alloc );
+
+run = function ( m, n, v ) {
+
+	var A, r, c;
+
+	A = alloc( m, n );
+
+	matrix.fill( A, 0, m, 0, n, v );
+
+	for ( r = 0 ; r < m ; ++r ) {
+
+		for ( c = 0 ; c < n ; ++c ) {
+
+			equal( A[r][c], v, r + "," + c + " : check value" );
+
+		}
+
+
+	}
+
+
+};
+
+
+test( "fill", function () {
+
+	run( 0, 0, 1 );
+	run( 1, 1, 2 );
+	run( 1, 2, 3 );
+	run( 2, 1, 4 );
+	run( 3, 3, 5 );
+	run( 3, 2, 6 );
+	run( 2, 3, 7 );
+
+} );
